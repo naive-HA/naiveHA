e1cdb74edce4f5e1ae4a9580f99e3ef8b9bf89bc
naive-HA <110729625+naive-HA@users.noreply.github.com>
2023-03-19 20:20:55 +1100

    Create led.py

---
+++ Proof-Of-Concept/Wired-Button/node/led.py
@@ -0,0 +1,40 @@
+from const import _LED_MODES
+from machine import Pin, Timer
+
+LED_MODES = _LED_MODES()
+
+class LED():
+    def __init__(self):
+        self.timer = Timer()
+        self.led = Pin("LED", Pin.OUT)
+        
+    def stop(self) -> None:
+        self.timer.deinit()
+        self.led.off()
+        return None
+    
+    def display(self, mode: int) -> None:
+        self.stop()
+        if mode == LED_MODES.NETWORK_DISCONNECTED:
+            self.led.off()
+            self.timer.init(freq=25, mode=Timer.PERIODIC, callback = lambda timer: self.led.toggle())
+            return None
+        elif mode == LED_MODES.NETWORK_CONNECTED:
+            self.stop()
+            return None
+        elif mode == LED_MODES.CONNECTING:
+            self.led.off()
+            self.timer.init(freq=2.5, mode=Timer.PERIODIC, callback = lambda timer: self.led.toggle())
+            return None
+        elif mode == LED_MODES.STOPPED_CONNECTING:
+            self.stop()
+            return None
+        else:
+            self.stop()
+            return None
+
+
+
+
+
+
